a145fdb89cacf1f822d613a5f5a9c8adab2272ba
parzival-516 <saraprajwal516@gmail.com>
2020-11-25 15:31:40 +0530

    otp added

---
+++ app/src/main/java/com/app/authenticationupgraded/otp.java
@@ -0,0 +1,14 @@
+package com.app.authenticationupgraded;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
